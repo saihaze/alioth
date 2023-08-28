08ee72d61754b93ffaebefb4aa390cc175ea5124
saihaze <develop@saihaze.cn>
2023-08-28 15:42:18 +0800

    Handle pointer click

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,12 @@
 use smithay::{
-    backend::input::{AbsolutePositionEvent, Event, InputBackend, InputEvent, KeyboardKeyEvent},
-    input::{keyboard::FilterResult, pointer::MotionEvent},
+    backend::input::{
+        AbsolutePositionEvent, ButtonState, Event, InputBackend, InputEvent, KeyboardKeyEvent,
+        PointerButtonEvent,
+    },
+    input::{
+        keyboard::FilterResult,
+        pointer::{ButtonEvent, MotionEvent},
+    },
     utils::SERIAL_COUNTER,
 };
 
@@ -42,6 +48,52 @@ impl State {
                         under,
                         &MotionEvent {
                             location: pos,
+                            serial,
+                            time: event.time_msec(),
+                        },
+                    );
+                }
+            }
+            InputEvent::PointerButton { event } => {
+                if let Some(pointer) = self.seat.get_pointer() {
+                    let button = event.button_code();
+                    let button_state = event.state();
+                    let serial = SERIAL_COUNTER.next_serial();
+
+                    if button_state == ButtonState::Pressed && !pointer.is_grabbed() {
+                        if let Some(window) = self
+                            .space
+                            .element_under(pointer.current_location())
+                            .map(|(w, _)| w.clone())
+                        {
+                            // Show the clicked window on the top.
+                            self.space.raise_element(&window, true);
+                            // If there is a keyboard, set its focus to the clicked window.
+                            if let Some(keyboard) = self.seat.get_keyboard() {
+                                keyboard.set_focus(
+                                    self,
+                                    Some(window.toplevel().wl_surface().clone()),
+                                    serial,
+                                );
+                            }
+                            for window in self.space.elements() {
+                                window.toplevel().send_pending_configure();
+                            }
+                        } else {
+                            for window in self.space.elements() {
+                                window.set_activated(false);
+                                window.toplevel().send_pending_configure();
+                            }
+                            if let Some(keyboard) = self.seat.get_keyboard() {
+                                keyboard.set_focus(self, None, serial);
+                            }
+                        }
+                    }
+                    pointer.button(
+                        self,
+                        &ButtonEvent {
+                            button,
+                            state: button_state,
                             serial,
                             time: event.time_msec(),
                         },
